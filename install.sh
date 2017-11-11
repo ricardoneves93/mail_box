965ca0fbc84a1c95e205aa99c089ed966971f78b
Ricardo Neves <ricardoneves93@hotmail.com>
2017-11-11 21:15:46 +0000

    Added installation instructions

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get install -y git
+sudo apt-get install -y mongodb
+sudo apt-get install -y pip
+cd /home/pi
+git clone https://github.com/ricardoneves93/mail_box.git
+sudo pip install pymongo
+sudo pip install pifcm
+# Install nodejs for ARM Chips
+wget http://node-arm.herokuapp.com/node_latest_armhf.deb 
+sudo dpkg -i node_latest_armhf.deb
